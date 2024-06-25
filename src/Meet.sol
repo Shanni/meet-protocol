1348198326225dbd1fc40f8b86481f21fa9f2b86
Shanni <shan.liu03@gmail.com>
2024-06-24 22:20:40 -0700

    edit contract

--- src/Meet.sol
+++ src/Meet.sol
@@ -2,23 +2,22 @@
 // Compatible with OpenZeppelin Contracts v5.0.0
 pragma solidity ^0.8.20;
 
-import {ERC721} from "lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MeetContract is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract MeetContract is ERC721URIStorage, ERC721Burnable, Ownable {
     uint256 private _nextTokenId;
+    string private _currentBaseURI; // Added to store the current base URI
+
+    uint256 public totalNumberOfTokens = 0;
 
     event TokenMinted(address indexed to, uint256 indexed tokenId, string uri);
     event TokenBurned(uint256 indexed tokenId);
 
-    constructor() ERC721("Meet", "MET") Ownable() {
-        _nextTokenId = 1; // Initializing the _nextTokenId to start from 1
-    }
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://www.example.com/"; // Added https for a more realistic URL
+    constructor() ERC721("Meet", "MET") Ownable(msg.sender) {
+        _nextTokenId = 1;
     }
 
     function safeMint(address to, string memory uri) public onlyOwner {
@@ -26,26 +25,38 @@ contract MeetContract is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
         emit TokenMinted(to, tokenId, uri);
+        totalNumberOfTokens++;
     }
 
-    function burn(uint256 tokenId) public override(ERC721Burnable, ERC721) {
+    function burn(uint256 tokenId) public override(ERC721Burnable) {
+        require(msg.sender == ownerOf(tokenId) || msg.sender == owner(), "Caller is not owner nor NFT owner");
         super.burn(tokenId);
         emit TokenBurned(tokenId);
+        totalNumberOfTokens--;
+    }
+
+    // Public function to update the base URI, restricted to the contract owner
+    function setBaseURI(string memory newBaseURI) public onlyOwner {
+        _currentBaseURI = newBaseURI;
     }
 
     function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public onlyOwner {
+        _setTokenURI(tokenId, _tokenURI);
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual override(ERC721URIStorage) {
+        super._setTokenURI(tokenId, _tokenURI);
+    }
+
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    // Ensures that functions in ERC721URIStorage and other inherited contracts are properly overridden
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
+    function _baseURI() internal view override returns (string memory) {
+        return _currentBaseURI;
     }
 }
